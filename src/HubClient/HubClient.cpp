77e00886dd595c6f3ca2759dc93978548e52818f
Giedrius Lukoševičius <giedriusl@halopowered.com>
2023-11-05 13:26:40 +0200

    add support for static IP

--- src/HubClient/HubClient.cpp
+++ src/HubClient/HubClient.cpp
@@ -8,10 +8,13 @@ HubClient::HubClient() : _localTimeHandler()
   timeStamp[0] = 0;
 }
 
-void HubClient::init(const char *ssid, const char *password)
+void HubClient::init(const char *ssid, const char *password, const char *staticIp, const char *gateway, const char *subnet)
 {
   _ssid = ssid;
   _password = password;
+  _staticIp = staticIp;
+  _gateway = gateway;
+  _subnet = subnet;
 }
 
 void HubClient::off()
@@ -27,12 +30,47 @@ void HubClient::on()
   // delay(1);
   WiFi.persistent(false);
   WiFi.mode(WIFI_STA);
-  WiFi.begin(_ssid, _password);
+  if (strlen(_staticIp))
+  {
+    IPAddress staticIP;
+    IPAddress gateway;
+    IPAddress subnet;
+
+    if (staticIP.fromString(_staticIp) && gateway.fromString(_gateway) && subnet.fromString(_subnet))
+    {
+      WiFi.config(staticIP, gateway, subnet);
+    }
+  }
+  _wifiMulti.addAP(_ssid, _password);
+  // WiFi.begin(_ssid, _password);
 }
 
 bool HubClient::isConnected()
 {
-  return WiFi.status() == WL_CONNECTED;
+  // return WiFi.status() == WL_CONNECTED;
+  return _wifiMulti.run(MAX_CONNECT_TIMEOUT) == WL_CONNECTED;
+}
+
+void HubClient::listNetworks()
+{
+  // WiFi.scanNetworks will return the number of networks found
+  Serial << F("Scanning networks\n");
+  int n = WiFi.scanNetworks();
+
+  if (n == 0)
+  {
+    Serial << F("No networks found\n");
+  }
+  else
+  {
+    Serial << n << F(" networks found\n");
+    for (int i = 0; i < n; ++i)
+    {
+      // Print SSID and RSSI for each network found
+      Serial << i + 1 << ": " << WiFi.SSID(i) << " (" << WiFi.RSSI(i) << ")\n";
+      delay(10);
+    }
+  }
 }
 
 bool HubClient::connect()
@@ -44,12 +82,44 @@ bool HubClient::connect()
     if (isConnected())
     {
       WiFi.localIP().toString().toCharArray(ip, 16);
+      Serial << F("MAC Address: ") << WiFi.macAddress() << "\n";
       return true;
     }
 
     delay(1000);
 
     Serial << ".\n";
+  }
+
+  Serial << "'" << _ssid << "' (" << _password << ")\n";
+
+  Serial << F("MAC Address: ") << WiFi.macAddress() << "\n";
+
+  listNetworks();
+
+  Serial << "Wifi status: ";
+  switch (_wifiMulti.run())
+  {
+  case WL_NO_SSID_AVAIL:
+    Serial << F("WL_NO_SSID_AVAIL\n");
+    break;
+  case WL_CONNECT_FAILED:
+    Serial << F("WL_CONNECT_FAILED\n");
+    // Turn radio OFF and delete AP config from NVS memory
+    Serial << F("Resetting WiFi\n");
+    WiFi.disconnect(true, true);
+    break;
+  case WL_IDLE_STATUS:
+    Serial << F("WL_IDLE_STATUS\n");
+    break;
+  case WL_CONNECTION_LOST:
+    Serial << F("WL_CONNECTION_LOST\n");
+    break;
+  case WL_DISCONNECTED:
+    Serial << F("WL_DISCONNECTED\n");
+    break;
+  default:
+    Serial << _wifiMulti.run() << "\n";
   }
 
   strcpy(ip, "-");
@@ -80,7 +150,7 @@ void HubClient::postMsgPack(const char *url, const uint8_t *payload, size_t size
   // dont use F() to reduce latency
   Serial << "Posting data to: " << url << "\n";
 
-  _http.begin(url);
+  _http.begin(_client, url);
   _http.addHeader(HEADER_CONTENT_TYPE, CONTENT_TYPE_MSG_PACK);
   int httpCode = _http.POST((uint8_t *)payload, size);
   if (httpCode != 201)
@@ -95,7 +165,7 @@ WiFiClient *HubClient::openMsgPackStream(const char *url, const char *ifModified
     return nullptr;
 
   _http.useHTTP10(true); // see https://arduinojson.org/v6/how-to/use-arduinojson-with-esp8266httpclient/
-  _http.begin(url);
+  _http.begin(_client, url);
   _http.addHeader(HEADER_ACCEPT, CONTENT_TYPE_MSG_PACK);
   if (ifModifiedSince && ifModifiedSince[0])
     _http.addHeader(HEADER_IF_MODIFIED_SINCE, ifModifiedSince);
